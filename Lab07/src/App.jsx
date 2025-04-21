c1a9701de64bcc8bbbac44ace2eb6dc710047193
MinhDuy <minhdii1510@gmail.com>
2025-04-21 13:32:24 +0700

    commit lab07 + minhchung

---
+++ Lab07/src/App.jsx
@@ -0,0 +1,13 @@
+import SummerRecipesUI from './components/SummerRecipesUI'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <SummerRecipesUI />
+    </>
+  )
+}
+
+export default App
